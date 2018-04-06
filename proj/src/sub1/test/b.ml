2c6e9d277c6498686b40a4ef428883f7b267ae1f
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-04-06 10:15:11 +0200

    Use alcotest in tests

--- proj/src/sub1/test/b.ml
+++ proj/src/sub1/test/b.ml
@@ -2,5 +2,9 @@
    Tests for Sub1.B
 *)
 
+let test_string () =
+  Alcotest.(check @@ neg string) "foo is not bar" "foo" "bar"
+
 let tests = [
+  "string", `Quick, test_string;
 ]
--- proj/src/test/run_tests.ml
+++ proj/src/test/run_tests.ml
@@ -2,33 +2,10 @@
    Run all the OCaml test suites defined in the project.
 *)
 
-let flatten_test_suites test_suites =
-  List.map (fun (suite_name, tests) ->
-    List.map (fun (test_name, test_fun) ->
-      (suite_name ^ "." ^ test_name, test_fun)
-    ) tests
-  ) test_suites
-  |> List.flatten
-
-let run_test name fun_ =
-  print_endline name;
-  fun_ ()
-
-(*
-   TODO: use a testing utility such as Alcotest, which will
-         catch exceptions and produce a clean report.
-*)
-let run_tests tests =
-  List.iter (fun (name, fun_) -> run_test name fun_) tests
-
-let test_suites = [
+let test_suites: unit Alcotest.test list = [
   "Sub1.A", Test_sub1.A.tests;
   "Sub1.B", Test_sub1.B.tests;
   "Sub2.A", Test_sub2.A.tests;
 ]
 
-let main () =
-  let tests = flatten_test_suites test_suites in
-  run_tests tests
-
-let () = main ()
+let () = Alcotest.run "proj" test_suites
