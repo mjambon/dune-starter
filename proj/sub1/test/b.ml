12ff137e2fe00937d6994134f4c6937b720a154e
Martin Jambon <martin@mjambon.com>
2018-04-11 19:24:54 -0700

    Put back `assert`-based hasty test for comparison with the proper Alcotest way.

--- proj/sub1/test/b.ml
+++ proj/sub1/test/b.ml
@@ -3,8 +3,12 @@
 *)
 
 let test_string () =
-  Alcotest.(check @@ neg string) "foo is not bar" "foo" "bar"
+  Alcotest.(check (neg string)) "foo is not bar" "foo" "bar"
+
+let test_string_hasty () =
+  assert ("foo" <> "bar")
 
 let tests = [
   "string", `Quick, test_string;
+  "string, hasty", `Quick, test_string_hasty;
 ]
